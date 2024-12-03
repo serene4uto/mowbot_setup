7d9dbebcfd4ab2a9335f35ffe180bba47adecdec
Zeki411 <nguyenhatrung411@gmail.com>
2024-12-03 22:27:09 +0900

    Run MowBotManage.py in the background and suppress output in demo script

--- scripts/AMR-MowBot-Demo.sh
+++ scripts/AMR-MowBot-Demo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export DISPLAY=:0.0
-python3 /home/farmbot/mowbot_project/mowbot_setup/py_scripts/MowBotManage.py
-# exit 0
+nohup python3 /home/farmbot/mowbot_project/mowbot_setup/py_scripts/MowBotManage.py > /dev/null 2>&1 &
+exit 0
